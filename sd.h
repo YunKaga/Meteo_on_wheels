e4b84326a2945452d1fb1b6a1bdfff60b286500a
YunKaga <voronzowyura@gmail.com>
2026-01-21 22:59:18 +0700

    сохранение маршрута в файл

--- sd.h
+++ sd.h
@@ -1,6 +1,9 @@
 #ifndef SD_H
 #define SD_H
+
 void sd_init();
-void sd_update(double sensors_data[]);
-void sd_save_now(double sensord_data[3]);
+void sd_log_motion(char cmd, unsigned long duration_ms);
+void sd_replay_route_reverse();
+bool sd_has_route();
+
 #endif
